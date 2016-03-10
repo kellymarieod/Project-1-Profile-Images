f147d766aa650c34076fc50753e273ab80398964
kellymarieod <kellymarieod@gmail.com>
2016-03-09 22:30:17 -0500

    made the moon footer js work and faded moon bottom

--- moonslide.js
+++ moonslide.js
@@ -1,19 +1,19 @@
 $(document).ready( function() {
-  var main-content = $(".text-box");
+  var pageContainer = $(".page-container");
   var footer = $(".footer");
   var offset = 0;
 
   $(window).scroll( function() {
-    offset = $(window).scrollTop() - main-content.offset().top;
-    // console.log(".text-box offset top " + textBox.offset().top);
+    offset = $(window).scrollTop() - pageContainer.offset().top;
+    // console.log(".page-container offset top " + pageContainer.offset().top);
     // console.log("window scroll top " + $(window).scrollTop());
-    console.log("offset " + offset);
+    // console.log("offset " + offset);
     if (offset > 100 && !$(footer).hasClass("shrink")) {
       footer.addClass("shrink");
-      console.log("added class of 'shrink' to .footer");
+      // console.log("added class of 'shrink' to .footer");
     } else if (offset <= 100 && footer.hasClass("shrink")) {
       footer.removeClass("shrink");
-      console.log("removed class of 'shrink' from .footer");
+      // console.log("removed class of 'shrink' from .footer");
     } else {
       return;
     }
